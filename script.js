083f634de2f59556d4a11e5dd86983b847b02a4c
Chuks <63956312+skido600@users.noreply.github.com>
2024-12-17 02:55:30 +0100

    menu mobile nav commit

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-//sidebar toggle
+const togglemenu = document.querySelector(".menu");
+const navbarRight = document.querySelector(".side-bar");
+const closeButton = document.querySelector(".close");
+console.log(togglemenu, navbarRight, closeButton);
+// Open sidebar when menu is clicked
+togglemenu.addEventListener("click", () => {
+  console.log("hi");
+  navbarRight.classList.add("active");
+});
 
-let sideBarOpen = false;
-let sidebar  = document.getElementById("side-bar");
-
-function openSideBar() {
-     if(!sideBarOpen){
-        sidebar.classList.add("side-responsive");
-        sideBarOpen = true;
-     }
-}
-
-function closeSidebar() {
-    if (sideBarOpen) {
-        sidebar.classList.remove("sidebar-responsive");
-        sideBarOpen = false;
-    }
-}
+// Close sidebar when close button is clicked
+closeButton.addEventListener("click", () => {
+  navbarRight.classList.remove("active");
+});
